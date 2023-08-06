b5fda9f1ccafe0f503c112d29f663f23d4aed40e
Vigneshwaran M <vignesh510510@gmail.com>
2023-08-06 17:29:29 +0530

    new features

--- backend/server.js
+++ backend/server.js
@@ -3,25 +3,87 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import cookieParser from 'cookie-parser';
-import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
+import cors from 'cors'; // Import the cors middleware
 const port = process.env.PORT || 5000;
-import adminRoutes from './routes/adminRoutes.js'
+import adminRoutes from './routes/adminRoutes.js';
 connectDB();
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
+app.use(cors({
+    origin: ['https://vwaran.me']
+  }));
+
 app.use('/api/admin', adminRoutes);
 
-app.get('/', (req,res) => res.send('Server is ready'));
+app.get('/', (req, res) => res.send('Server is ready'));
 app.use(notFound);
 app.use(errorHandler);
 
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
-app.listen(port, () => console.log(`server is running on port ${port}`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from 'express';
+// import dotenv from 'dotenv';
+// dotenv.config();
+// import cookieParser from 'cookie-parser';
+// import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+// import connectDB from './config/db.js';
+// const port = process.env.PORT || 5000;
+// import adminRoutes from './routes/adminRoutes.js'
+// connectDB();
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true}));
+
+// app.use(cookieParser());
+
+// app.use('/api/admin', adminRoutes);
+
+// app.get('/', (req,res) => res.send('Server is ready'));
+// app.use(notFound);
+// app.use(errorHandler);
+
+
+
+// app.listen(port, () => console.log(`server is running on port ${port}`));
