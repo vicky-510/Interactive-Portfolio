f7d84528beccd42e754d3230dbb8efed58d49b7c
Vigneshwaran M <vignesh510510@gmail.com>
2023-08-06 17:35:39 +0530

    new features

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['https://vwaran.me']
+    origin:'https://vwaran.me/'
   }));
 
 app.use('/api/admin', adminRoutes);
